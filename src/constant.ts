8b87aa978a8644436affa93dc540597ee86db064
LiYuqing <im.yuqlee@gmail.com>
2022-07-06 17:03:36 +0800

    Add whitelist assets

--- src/constant.ts
+++ src/constant.ts
@@ -6,3 +6,12 @@ export const BRIDGE_ADDRESS = '0x12266b2BbdEAb152f8A0CF83c3997Bc8dbAD0be0'
 export const MVM_RPC_URI = 'https://geth.mvm.dev/'
 
 export const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const WHITELIST = {
+  'c94ac88f-4671-3976-b60a-09064f1811e8': true, // xin
+  'c6d0c728-2624-429b-8e0d-d9d19b6592fa': true, // btc
+  '43d61dcd-e413-450d-80b8-101d5e903357': true, // eth
+  'eea900a8-b327-488c-8d8d-1428702fe240': true, // mob
+  '25dabac5-056a-48ff-b9f9-f67395dc407c': true, // tron
+  '6cfe566e-4aad-470b-8c9a-2fd35b49c68d': true, // eos
+}
