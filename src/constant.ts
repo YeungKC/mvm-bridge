72d004ec201fbeddfdaa9445022ae9323382aac3
YeungKC <I@YeungKC.com>
2022-07-07 09:16:18 +0800

    Change assets list size, list item show balance

--- src/constant.ts
+++ src/constant.ts
@@ -7,11 +7,14 @@ export const MVM_RPC_URI = 'https://geth.mvm.dev/'
 
 export const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
 
-export const WHITELIST = {
-  'c94ac88f-4671-3976-b60a-09064f1811e8': true, // xin
-  'c6d0c728-2624-429b-8e0d-d9d19b6592fa': true, // btc
-  '43d61dcd-e413-450d-80b8-101d5e903357': true, // eth
-  'eea900a8-b327-488c-8d8d-1428702fe240': true, // mob
-  '25dabac5-056a-48ff-b9f9-f67395dc407c': true, // tron
-  '6cfe566e-4aad-470b-8c9a-2fd35b49c68d': true, // eos
-}
+export const WHITELIST = [
+  'c6d0c728-2624-429b-8e0d-d9d19b6592fa', // btc
+  '4d8c508b-91c5-375b-92b0-ee702ed2dac5', // usdt
+  'eea900a8-b327-488c-8d8d-1428702fe240', // mob
+  '43d61dcd-e413-450d-80b8-101d5e903357', // eth
+  '25dabac5-056a-48ff-b9f9-f67395dc407c', // trx
+  '31d2ea9c-95eb-3355-b65b-ba096853bc18', // pusd
+  'f5ef6b5d-cc5a-3d90-b2c0-a2fd386e7a3c', // box
+  'c94ac88f-4671-3976-b60a-09064f1811e8', // xin
+  '965e5c6e-434c-3fa9-b780-c50f43cd955c', // cnb
+]
--- src/page/Assets.tsx
+++ src/page/Assets.tsx
@@ -1,11 +1,17 @@
-import { memo } from 'react'
+import { AssetResponse } from '@mixin.dev/mixin-node-sdk'
+import { memo, useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
 import RoundedItem from '../components/RoundedItem'
-import { useTopAssets } from '../service/hook'
+import { WHITELIST } from '../constant'
+import { useTopAssets, useMvmBalance } from '../service/hook'
 
 export const Assets = memo(() => {
   const { data } = useTopAssets()
+  const filetedList = useMemo(
+    () => data?.filter(({ asset_id }) => WHITELIST.includes(asset_id)),
+    [data],
+  )
 
   return (
     <div className='container flex flex-col items-stretch gap-2 p-4 py-20'>
@@ -14,16 +20,29 @@ export const Assets = memo(() => {
           All Depositing
         </RoundedItem>
       </div>
-      <div className=' text-lg font-bold'>Top Assets</div>
+      <div className=' text-lg font-bold'>Assets</div>
       <div>
-        {data?.map((e, i) => (
-          <RoundedItem key={e.asset_id} $as={Link} to={`/asset/${e.asset_id}`}>
-            <img alt='icon' src={e.icon_url} width={38} height={38} />
-            <div className='font-bold'>{e.symbol}</div>
-          </RoundedItem>
+        {filetedList?.map((e) => (
+          <AssetItem key={e.asset_id} asset={e} />
         ))}
       </div>
     </div>
+  )
+})
+
+const AssetItem = memo(({ asset }: { asset: AssetResponse }) => {
+  const { data } = useMvmBalance(asset.asset_id)
+
+  return (
+    <RoundedItem $as={Link} to={`/asset/${asset.asset_id}`}>
+      <img alt='icon' src={asset.icon_url} width={38} height={38} />
+      <div className='grow font-bold'>{asset.symbol}</div>
+      {data && (
+        <div className='font-semibold'>
+          {data.formatted} {asset.symbol}
+        </div>
+      )}
+    </RoundedItem>
   )
 })
 
