11e8237ba2482097262d1b96ccfd448cd45a5e54
YeungKC <I@YeungKC.com>
2022-07-15 12:30:02 +0800

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:@typescript-eslint/strict',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
